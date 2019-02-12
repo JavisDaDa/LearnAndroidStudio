39d6ddfe8123e7ea4961e5dd437eabe7766641f0
JavisDaDa <269423231@qq.com>
2019-02-12 16:53:00 +0800

    Project1&2

---
+++ ActivityTest/app/src/main/java/com/example/mikejia/activitytest/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.mikejia.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "You clicked Button1", Toast.LENGTH_LONG).show();//创建一个toast
+                //finish();
+
+//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                Intent intent = new Intent("com.example.mikejia.activitytest.ACTION_START");
+//                intent.addCategory("com.example.mikejia.activitytest.MY_CATEGORY");
+//                startActivity(intent);
+
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent);
+
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel: 10086"));
+//                startActivity(intent);
+
+//                String data = "Hello SecondActivity";
+//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                intent.putExtra("extra_data", data);
+//                startActivity(intent);
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case 1:
+                if (requestCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("MainActivity", returnedData);
+                }
+                break;
+            default:
+        }
+    }
+}
+
